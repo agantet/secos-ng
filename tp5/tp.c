9c7a1f96ce7b9768b3fddccab24ef33d4e07b69d
AGantet <anais.gantet.pro@gmail.com>
2023-10-07 23:19:48 +0200

    [code] tp.c in tp5 updated

--- tp5/tp.c
+++ tp5/tp.c
@@ -1,6 +1,19 @@
 /* GPLv2 (c) Airbus */
 #include <debug.h>
 
+void syscall_isr() {
+   asm volatile (
+      "leave ; pusha        \n"
+      "mov %%esp, %eax      \n"
+      "call syscall_handler \n"
+      "popa ; iret"
+      );
+}
+
+void __regparm__(1) syscall_handler(int_ctx_t *ctx) {
+   debug("SYSCALL eax = %p\n", ctx->gpr.eax);
+}
+
 void userland() {
     // TODO à compléter
    while(1);
